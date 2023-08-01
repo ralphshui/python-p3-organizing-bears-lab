00c24c7f8c799c7729fab89d8515605a3059da26
Ralph Shui <rskc.2112@gmail.com>
2023-07-31 23:06:17 -0400

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Mr. Chocolate', 3, 'M', 'Black', 'Chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Rowdy', 5, 'M', 'White', 'Angry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Tabitha', 3, 'F', 'Black', 'Mean', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Sergeant Brown', 9, 'F', 'Brown', 'Chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Melissa', 8, 'F', 'Black', 'Asleep', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Grinch', 10, 'F', 'Black', 'Chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ('Wendy', 14, 'F', 'Brown', 'Hungry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES (NULL, 6, 'M,', 'White', 'Angry', 1);
